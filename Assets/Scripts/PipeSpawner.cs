36f892b944fb6a6f58a2f4f5866434750f8160ce
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-02 00:04:23 +0200

    Update PipeSpawner.cs

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -1,23 +1,19 @@
 using UnityEngine;
 
-public class PipeSpawner : MonoBehaviour
-{
+public class PipeSpawner : MonoBehaviour{
     public GameObject PipePrefab;
     [SerializeField] private float spawnInterval = 2f; 
     [SerializeField] private float minHeightOffset = -3f;
     [SerializeField] private float maxHeightOffset = 3f; 
     [SerializeField] private float horizontalSpacing = 5f;
-
     private Vector3 lastSpawnPosition;
 
-    private void Start()
-    {
+    private void Start(){
         lastSpawnPosition = transform.position;
         InvokeRepeating(nameof(SpawnPipe), 0f, spawnInterval);
     }
 
-    private void SpawnPipe()
-    {
+    private void SpawnPipe(){
         float randomHeight = Random.Range(minHeightOffset, maxHeightOffset);
         Vector3 spawnPosition = new Vector3(lastSpawnPosition.x + horizontalSpacing, transform.position.y + randomHeight, transform.position.z);
         Instantiate(PipePrefab, spawnPosition, Quaternion.identity);
