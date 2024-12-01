ff2a8c156aafd72146580dbcfe68537289c6655f
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2024-12-02 00:03:08 +0200

    Update GameOver.cs

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -3,11 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour{
     [SerializeField] string sceneName = "GameOver"; 
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Pipe")) 
-        {
+    
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Pipe")) {
             Debug.Log("Collided with pipe, loading scene: " + sceneName);
             SceneManager.LoadScene(sceneName); 
         }
